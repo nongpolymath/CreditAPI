59ccdfb4650edb07f79d9258d2cc4a5031b32ee2
polymath7 <73620293+nongpolymath@users.noreply.github.com>
2020-12-10 14:57:33 -0600

    add pom dependencies and credit apply class

--- src/test/CreditApply.java
+++ src/test/CreditApply.java
@@ -10,6 +10,105 @@ import javax.ws.rs.core.Application;
 
 @Path("/credit")
 public class CreditApply {
+	private int ssn;
+	private String firstName;
+	private String lastName;
+	private int annual_income;
+	
+	public CreditApply() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public CreditApply(int ssn, String firstName, String lastName, int annual_income) {
+		super();
+		this.ssn = ssn;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.annual_income = annual_income;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + annual_income;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ssn;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CreditApply other = (CreditApply) obj;
+		if (annual_income != other.annual_income)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (ssn != other.ssn)
+			return false;
+		return true;
+	}
+
+
+	public int getSsn() {
+		return ssn;
+	}
+
+
+	public void setSsn(int ssn) {
+		this.ssn = ssn;
+	}
+
+
+	public int getAnnual_income() {
+		return annual_income;
+	}
+
+
+	public void setAnnual_income(int annual_income) {
+		this.annual_income = annual_income;
+	}
+
+
 	@GET
 	@Produces(MediaType.TEXT_XML)
 	public String sayServerResponse() {
